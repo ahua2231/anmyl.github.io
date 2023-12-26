2e874c5e2a6c71e5d086c3ec2bf3b29de6a700a8
ahua2231 <154927189+ahua2231@users.noreply.github.com>
2023-12-27 05:09:27 +0800

    Add files via upload

---
+++ js/js/cookie.js
@@ -0,0 +1,25 @@
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+
+function getCookie(name) {
+    var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr != null) {
+        return unescape(arr[2]);
+    } else {
+        return null;
+    }
+}
+
+function delCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null) {
+        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
