6e74169a74819e164d6b2f7b9351cd894c96fe34
ahua2231 <154927189+ahua2231@users.noreply.github.com>
2024-02-07 09:49:11 +0800

    Update dmaku.js

--- js/dmaku.js
+++ js/dmaku.js
@@ -36,8 +36,53 @@ $(function() {
         }
     });
 });
-var pagePaths = ['iframe-web/index.html', 'iframe-web/image.html', 'iframe-web/link.html', 'iframe-web/Speech-skills.html'];
+// 路由处理函数
+function handleRoutes() {
+    const hash = window.location.hash.substring(1);
+    // 获取URL中的哈希值
+    let pageUrl = '';
+    let pageTitle = '';
 
-function changePage(index) {
-    document.getElementById('contentFrame').src = pagePaths[index];
+    switch (hash) {
+    case '/page1':
+        pageUrl = 'iframe-web/link.html';
+        pageTitle = 'MIPAY-链接';
+        break;
+    case '/page2':
+        pageUrl = 'iframe-web/image.html';
+        pageTitle = 'MIPAY-VPAY-步骤图';
+        break;
+    case '/page3':
+        pageUrl = 'iframe-web/Speech-skills.html';
+        pageTitle = 'MIPAY-语录';
+        break;
+    default:
+        pageUrl = 'iframe-web/link.html';
+        // 默认加载第一页
+        pageTitle = 'MIPAY-链接';
+    }
+
+    // 发起 Ajax 请求获取页面内容
+    fetch(pageUrl).then(response=>{
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    }
+    ).then(data=>{
+        // 设置 iframe 的内容为 Ajax 获取的页面内容
+        document.getElementById('contentFrame').srcdoc = data;
+        // 设置页面标题
+        document.title = pageTitle;
+    }
+    ).catch(error=>{
+        console.error('Error fetching page:', error);
+    }
+    );
 }
+
+// 页面加载完成时立即执行一次路由处理函数
+document.addEventListener('DOMContentLoaded', handleRoutes);
+
+// 监听URL的变化，并在变化时执行路由处理函数
+window.addEventListener('hashchange', handleRoutes);
